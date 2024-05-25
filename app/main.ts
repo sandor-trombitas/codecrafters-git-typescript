9bbd9bde7437b1fed99582d841a9993b575a901b
Sandor Trombitas <tis.sandor@gmail.com>
2024-05-25 15:30:46 +0300

    CatFile

--- app/main.ts
+++ app/main.ts
@@ -1,11 +1,37 @@
 import * as fs from "fs";
+import zlib from "zlib";
 
 const args = process.argv.slice(2);
 const command = args[0];
 
 enum Commands {
   Init = "init",
+  CatFile = "cat-file",
 }
+
+const catFile = (args: Array<string>) => {
+  const params = args.at(1);
+  const hash = args.at(2);
+  if (params?.indexOf("-p") !== 0) {
+    throw new Error(
+      "fatal: only two arguments allowed in <type> <object> mode, not 1",
+    );
+  }
+  if (!hash) {
+    throw new Error(`fatal: <object> required with ${params}`);
+  }
+  const directory = hash.substring(0, 2);
+  const file = hash.substring(2);
+  const blob = fs.readFileSync(`.git/objects/${directory}/${file}`);
+  const decompressedBuffer = zlib.unzipSync(blob);
+
+  // The format of a blob object file looks like this (after Zlib decompression):
+  // `blob <size>\0<content>`
+  // We want to output the content
+  process.stdout.write(
+    decompressedBuffer.subarray(decompressedBuffer.indexOf(0) + 1).toString(),
+  );
+};
 
 switch (command) {
   case Commands.Init:
@@ -18,6 +44,9 @@ switch (command) {
     fs.mkdirSync(".git/refs", { recursive: true });
     fs.writeFileSync(".git/HEAD", "ref: refs/heads/main\n");
     console.log("Initialized git directory");
+    break;
+  case Commands.CatFile:
+    catFile(args);
     break;
   default:
     throw new Error(`Unknown command ${command}`);
